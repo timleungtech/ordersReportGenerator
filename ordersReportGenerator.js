1e7ca6618a22e700d3c1a21b19c361c47c2b263d
timleungtech <timleungtech@gmail.com>
2026-03-28 22:52:32 -0400

    remove comments

--- ordersReportGenerator.js
+++ ordersReportGenerator.js
@@ -144,35 +144,3 @@ function createSubtotalsRow(){
     console.log(`Filled: ${totalFillQty}`);
 }
 createSubtotalsRow()
-// // inserts a div before the end of the DOM body tag
-// function insertContainerInDom(){
-//     document.querySelector('body').insertAdjacentHTML('beforeEnd', '<div id="ordersbyproduct-container"></div>')
-// }
-// // creates an HTML table in DOM
-// function createHTMLtable(){
-//     let rows = createSubtotalsRow()
-//     let html = '<table>'
-//     html += '<tr>'
-//     for(let j in rows[0]) {
-//         html += '<th>' + j + '</th>'
-//     }
-//     html += '</tr>'
-//     for(let i = 0; i < rows.length; i++) {
-//         html += '<tr>'
-//         for(let j in rows[i]) {
-//             if(rows[i].customer === null){
-//                 html += '<th>' + rows[i][j] + '</th>'
-//             } else {
-//                 html += '<td>' + rows[i][j] + '</td>'
-//             }        
-//         }
-//         html += '</tr>'
-//     }
-//     html += '</table>'
-//     document.getElementById('ordersbyproduct-container').innerHTML = html
-// }
-// function generateTableInDOM(){
-//     insertContainerInDom()
-//     createHTMLtable()
-// }
-// document.querySelector('#button-1227').addEventListener('click', generateTableInDOM())
